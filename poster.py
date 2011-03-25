4bdffbb5b1f71e86f0ed30c6c99603fc582b910f
Nils B <nbnet@arcor.de>
2011-03-25 17:34:41 +0100

    download tiles

--- poster.py
+++ poster.py
@@ -1,4 +1,4 @@
-import Image, ImageDraw, sys
+import Image, ImageDraw, sys, os
 
 global application_name, version_number, cmd_name
 global output_filename, top_tile, left_tile, height, width, zoom, style
@@ -10,12 +10,29 @@ version_number = "0.0.1"
 server_url = "http://c.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707"	
 tilesize = 256
 
-def download_tiles(top_tile, left_tile, height, width, zoom, style):
-	prefix_url = "/" + str(style) + "/" + str(tilesize) + "/" + str(zoom)
+def execute_cmd(action, cmd):
+	sys.stdout.write(action + "...")
+	sys.stdout.flush()
 	
+	value = os.system(cmd)
+	if value != 0: raise Exception(action + " failed.") 
+	print " FINISHED"
+
+def clean():
+	print "Deleting temporary files..."
+	if os.path.isdir("tiles"):
+		execute_cmd("Deleting tiles folder", "rm -R tiles")
+
+def download_tiles(top_tile, left_tile, height, width, zoom, style):
+	prefix_url = str(style) + "/" + str(tilesize) + "/" + str(zoom)
+	clean()
+	execute_cmd("Creating tiles folder", "mkdir tiles")
+
+	print "Downloading tiles..."
 	for x in range(0, int(height)/tilesize):
 		for y in range(0, int(width)/tilesize):
-			print x,y
+			url = server_url + "/" + prefix_url + "/" + str(int(top_tile) + y) + "/" + str(int(left_tile) + x) + ".png"
+			execute_cmd("Downloading " + url, "wget -O tiles/"+ str(x) + "_" + str(y) + ".png " + url)
 
 def generate_poster(height, width, output_filename):
 	return
@@ -46,6 +63,7 @@ if len(sys.argv) == 8:
 	
 	download_tiles(top_tile, left_tile, height, width, zoom, style)
 	generate_poster(height, width, output_filename)
+   #clean()
 elif len(sys.argv) == 2:
 	option = sys.argv[1]
 	if option == "-v": version()
